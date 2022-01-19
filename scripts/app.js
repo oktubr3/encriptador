83ba70c03172e921eaedddc87f65524b97175111
oktubr3 <mauroh@gmail.com>
2022-01-18 23:56:51 -0300

    Va queriendo

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,58 @@
+/* Reglas de encriptación: 
+'e' es convertido para 'enter' 
+'i' es convertido para 'imes'
+'a' es convertido para 'ai'
+'o' es convertido para 'ober'
+'u' es convertido para 'ufat'
+Solo letras minusculas
+No se permite acentuación de palabras 
+*/
 
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
+const encripta = texto => texto
+    .replaceAll( 'e', 'enter' )
+    .replaceAll( 'i', 'imes' )
+    .replaceAll( 'a', 'ai' )
+    .replaceAll( 'o', 'ober' )
+    .replaceAll( 'u', 'ufat' );
 
-const encriptadorAcme = (texto) => {
-    encriptado = texto
-        .replaceAll(/a/g, 'ai')
-        .replaceAll(/e/g, 'enter')
-        .replaceAll(/i/g, 'imes')
-        .replaceAll(/o/g, 'ober')
-        .replaceAll(/u/g, 'ufat')
-    return encriptado;
+
+console.log( encripta ('hola como estas') )
+
+
+/* Reglas de desencriptación: 
+'enter' es convertido para 'e' 
+'imes' es convertido para 'i'
+'ai' es convertido para 'a'
+'ober' es convertido para 'o'
+'ufat' es convertido para 'u'
+Solo letras minusculas
+No se permite acentuación de palabras   
+*/
+
+const desencripta = texto => {
+    for (let i = 0; i < texto.length; i++) {
+        texto = texto.replace( 'enter', 'e' );
+        texto = texto.replace( 'imes', 'i' );
+        texto = texto.replace( 'ai', 'a' );
+        texto = texto.replace( 'ober', 'o' );
+        texto = texto.replace( 'ufat', 'u' );
+                
+            
+    }
+    return texto;
 }
 
-const desencriptadorAcme = (texto) => {
-    desencriptado = texto
-        .replaceAll(/ai/g, 'a')
-        .replaceAll(/enter/g, 'e')
-        .replaceAll(/imes/g, 'i')
-        .replaceAll(/ober/g, 'o')
-        .replaceAll(/ufat/g, 'u')
-    return desencriptado;
+console.log(desencripta('hoberlai cobermober enterstais'))
+
+
+// Función de copiar 
+
+function copy() {
+    var copyText = document.querySelector('#msg');
+    copyText.select();
+    document.execCommand('copy');
 }
 
-console.log(encriptadorAcme('hola'))
+document.querySelector('#copy').addEventListener('click', copy);
 
-console.log(desencriptadorAcme('hoberlaimes'))
+
