613c3c7bd1d6eb902584eccb6c16758fc70748c2
oktubr3 <mauroh@gmail.com>
2022-01-20 20:13:45 -0300

    falta afinar

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,10 @@
+const input = document.querySelector('#textAreaInput').value;
+let output = document.querySelector('#textAreaOutput');
+const btnEncript = document.querySelector('#btnEncript');
+const btnDecript = document.querySelector('#btnDecript');
+const btnCopy = document.querySelector('#btnCopy');
+const btnDelete = document.querySelector('#btnDelete');
+
 /* Reglas de encriptación: 
 'e' es convertido para 'enter' 
 'i' es convertido para 'imes'
@@ -9,34 +16,51 @@ No se permite acentuación de palabras
 */
 
 // Encriptado
-const encripta = texto => texto
+btnEncript.addEventListener("click", (e) =>  {
+e.preventDefault()  // Evita que se recargue la página
+const encriptado = input
     .replaceAll( 'e', 'enter' )
     .replaceAll( 'i', 'imes' )
     .replaceAll( 'a', 'ai' )
     .replaceAll( 'o', 'ober' )
     .replaceAll( 'u', 'ufat' );
+output.value = encriptado;
+});
 
 
-console.log( encripta ('hola como estas') )
 
 // Desencriptado
-const desencripta = texto => texto
+btnDecript.addEventListener("click", (e) => {
+e.preventDefault()  // Evita que se recargue la página
+const desencriptado = input
     .replaceAll( 'enter', 'e' )
     .replaceAll( 'imes', 'i' )
     .replaceAll( 'ai', 'a' )
-    .replaceAll( 'ober', 'o' )
+    .replaceAll( 'ober', 'o' ) 
+    .replaceAll( 'ufat', 'u' );
+output.value = desencriptado;
+});
 
-console.log(desencripta('hoberlai cobermober enterstais'))
-
+btnDelete.addEventListener("click", (e) => {
+e.preventDefault()  // Evita que se recargue la página
+output.value = '';
+});
 
 // Función de copiar 
-
 const copy = () => {
-    let copyText = document.querySelector('#msg');
+    let copyText = document.querySelector('#textAreaOutput');
     copyText.select();
     document.execCommand('copy');
+    clean(output);
+    
 }
 
-document.querySelector('#copy').addEventListener('click', copy);
+function clean(output) {
+    output.value = "Copiado...";
+    setTimeout(() => {
+        output.value = "";
+    }, 500);
+}
 
+document.querySelector('#btnCopy').addEventListener('click', copy);
 
