b1692758240e9da8afa94424f28c3343903cff75
oktubr3 <mauroh@gmail.com>
2022-01-21 10:26:15 -0300

    Terminado

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,9 @@
-const input = document.querySelector('#textAreaInput').value;
+const input = document.querySelector('#textAreaInput');
 let output = document.querySelector('#textAreaOutput');
 const btnEncript = document.querySelector('#btnEncript');
 const btnDecript = document.querySelector('#btnDecript');
 const btnCopy = document.querySelector('#btnCopy');
 const btnDelete = document.querySelector('#btnDelete');
-
 
 /* Reglas de encriptación: 
 'e' es convertido para 'enter' 
@@ -17,79 +16,67 @@ No se permite acentuación de palabras
 */
 
 // Encriptado
-btnEncript.addEventListener("click", (e) =>  {
-e.preventDefault()  // Evita que se recargue la página
-const encriptado = input
-    .replaceAll( 'e', 'enter' )
-    .replaceAll( 'i', 'imes' )
-    .replaceAll( 'a', 'ai' )
-    .replaceAll( 'o', 'ober' )
-    .replaceAll( 'u', 'ufat' );
-output.value = encriptado;
+btnEncript.addEventListener('click', (e) => {
+    e.preventDefault()  // Evita que se recargue la página
+    let text = input.value;
+    validarInput();  // Valida que el input no tenga caracteres especiales   
+    if (valido) {
+        output.value = text
+            .replaceAll('e', 'enter')
+            .replaceAll('i', 'imes')
+            .replaceAll('a', 'ai')
+            .replaceAll('o', 'ober')
+            .replaceAll('u', 'ufat');
+    }
 });
-
-
 
 // Desencriptado
 btnDecript.addEventListener("click", (e) => {
-e.preventDefault()  // Evita que se recargue la página
-const desencriptado = input
-    .replaceAll( 'enter', 'e' )
-    .replaceAll( 'imes', 'i' )
-    .replaceAll( 'ai', 'a' )
-    .replaceAll( 'ober', 'o' ) 
-    .replaceAll( 'ufat', 'u' );
-output.value = desencriptado;
+    e.preventDefault()  // Evita que se recargue la página
+    let text = input.value;
+    validarInput();  // Valida que el input no tenga caracteres especiales
+    if(valido){
+        output.value = text
+            .replaceAll('enter', 'e')
+            .replaceAll('imes', 'i')
+            .replaceAll('ai', 'a')
+            .replaceAll('ober', 'o')
+            .replaceAll('ufat', 'u');
+    }
 });
 
+// Eliminar texto
 btnDelete.addEventListener("click", (e) => {
-e.preventDefault()  // Evita que se recargue la página
-output.value = '';
+    e.preventDefault()  // Evita que se recargue la página
+    output.value = '';
 });
 
-// Función de copiar 
+// Copiar al portapapeles
 const copy = () => {
-    let copyText = document.querySelector('#textAreaOutput');
-    copyText.select();
+    output.select();
     document.execCommand('copy');
-    clean(output);
+    copiado();
 }
-document.querySelector('#btnCopy').addEventListener('click', copy);
+btnCopy.addEventListener('click', copy);
 
-const clean = (output) => {
+// Mensaje de copiado
+const copiado = () => {
     output.value = "Copiado...";
     setTimeout(() => {
         output.value = "";
     }, 500);
 }
 
-// Ejemplo
-// /*--------------------------------ENCRIPTAR MENSAJE-------------------------------*/
-// document.querySelector("#btn-encriptar").addEventListener("click", function (event) {
-//     event.preventDefault();
-//     var texto = document.querySelector("#input-texto").value;
-//     var textCodif = texto.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
-//     document.querySelector("#msg").value = textCodif;
-// });
+// Validar input
+let valido = true;
 
-// /*------------------------------DESENCRIPTAR MENSAJE-------------------------------*/
-// document.querySelector("#btn-desencriptar").addEventListener("click", function (event) {
-//     event.preventDefault();
-//     var texto = document.querySelector("#input-texto").value;
-//     var textDecod = texto.replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ai/gi, 'a').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
-//     document.querySelector("#msg").value = textDecod;
+validarInput = () => {
+    const regexp = /^[a-z0-9 ]+$/;
+    if (!regexp.test(input.value)) {
+        alert('No se permiten caracteres especiales ni mayusculas!');
+        input.value = '';
+        output.value = '';
+        valido = false;
+    }
 
-// });
-
-// /*---------------------------------BOTÓN COPIAR-------------------------------*/
-// document.querySelector("#btn-copy").addEventListener("click", function (event) {
-//     event.preventDefault();
-//     var copiarText = document.querySelector("#msg"); 
-//     copiarText.select();
-//     document.execCommand("copy");
-// });
-
-
-
-
-
+}
